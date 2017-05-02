07031d03f20faf495291f920bfcd22e9ac685201
Dimitrios Liappis <dimitrios.liappis@elastic.co>
2017-05-02 11:56:45 +0300

    Relax test for setting heap size via env var
    When specifying max heap size with -Xmx the reported value differs
    slightly from the specified size. This is due to the JVM doing some
    calculations for generation ratios, correcting alignment and rounding
    the result[1], so we need to be less explicit about this test. It is
    good enough if ES reports the max heap size within a delta(set to
    64MB) of the specified size.
    
    [1] http://stackoverflow.com/a/28395673

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -15,11 +15,16 @@ def test_setting_cluster_name_with_an_environment_variable(elasticsearch):
 def test_setting_heapsize_with_an_environment_variable(elasticsearch):
     # The fixture for this test comes from tests/docker-compose.yml.
     #
-    # The number of bytes that we assert is not exactly what we specify
-    # in the fixture. The exact value would be 1206910976, but due to JVM
-    # subtleties, the actual number is lower.
+    # The number of bytes that we assert is not exactly what we
+    # specify in the fixture. This is due to jvm honoring the
+    # generation and survivor ratios, rounding and alignments.  It is
+    # enough if Elasticsearch reports a max heap size within 64MB of
+    # the target value set in the fixture (=1152MB).
+
+    mem_delta_mb = 64
     for jvm in elasticsearch.get_node_jvm_stats():
-        assert jvm['mem']['heap_max_in_bytes'] == 1173094400
+        reported_heap_max_in_mb = int(jvm['mem']['heap_max_in_bytes'] / (1024**2))
+        assert abs(reported_heap_max_in_mb - 1152) < mem_delta_mb
 
 
 def test_envar_not_including_a_dot_is_not_presented_to_elasticsearch(elasticsearch):
