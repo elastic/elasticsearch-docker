f76bba08c1a8cfd3c821f641adb2b10e3cfa47b9
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2017-08-01 13:25:50 +0300

    Add acceptance test to ensure image doesn't contain core files in /
    In some occasions, depending on the build platform (noticed with aufs with old docker-ce versions) may create a /corefile.<pid>.
    
    Fail a build if the produced image containers any /core* files.
    
    Relates #97

--- tests/test_base_os.py
+++ tests/test_base_os.py
@@ -10,3 +10,9 @@ def test_java_home_env_var(host):
     java_path_cmdline = '$JAVA_HOME/bin/java -version'
 
     assert host.run(java_path_cmdline).exit_status == 0
+
+
+def test_no_core_files_exist_in_root(host):
+    core_file_check_cmdline = 'ls -l /core*'
+
+    assert host.run(core_file_check_cmdline).exit_status != 0
