48ab725770843432baeb6bf8584132b3ac5ef0f6
Toby McLaughlin <toby@jarpy.net>
2018-04-09 09:25:44 +0200

    Support open X-Pack

--- tests/test_es_plugins.py
+++ tests/test_es_plugins.py
@@ -2,15 +2,6 @@ from .fixtures import elasticsearch
 import pytest
 from requests import codes
 
-image_flavor = pytest.config.getoption('--image-flavor')
-
-
-@pytest.mark.skipif(image_flavor == 'oss',
-                    reason="x-pack not installed in the -{} image.".format(image_flavor))
-def test_uninstall_xpack_plugin(elasticsearch):
-    # Ensure plugins can be uninstalled, see https://github.com/elastic/elasticsearch/issues/24231
-    assert elasticsearch.uninstall_plugin("x-pack").exit_status == 0
-
 
 def test_IngestUserAgentPlugin_is_installed(elasticsearch):
     # Ensure IngestUserAgentPlugin is present on all nodes
