94aec3f6fe621f800307c60225afd6d4870aac4d
Toby McLaughlin <toby@jarpy.net>
2018-06-21 13:59:49 +1000

    Use Mozilla CA cert bundle from the OS for OpenJDK
    Fixes #171

---
+++ tests/test_java.py
@@ -0,0 +1,17 @@
+def test_java_is_runnable_via_java_home_env_var(host):
+    assert host.run('$JAVA_HOME/bin/java -version').exit_status == 0
+
+
+def test_java_major_version(host):
+    assert 'openjdk version "10.' in host.run('$JAVA_HOME/bin/java -version').stderr
+
+
+def test_java_uses_the_os_provided_keystore(host):
+    realpath = host.run('realpath $JAVA_HOME/lib/security/cacerts').stdout.strip()
+    assert realpath == '/etc/pki/ca-trust/extracted/java/cacerts'
+
+
+def test_amazon_ca_certs_are_in_the_keystore(host):
+    cmd = '$JAVA_HOME/bin/keytool -cacerts -storepass changeit -list | grep trustedCertEntry'
+    certs = host.run(cmd).stdout.split('\n')
+    assert any(['amazonrootca' in cert for cert in certs])
