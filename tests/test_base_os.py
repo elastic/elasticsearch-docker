f32514bb10691aedb6450b5d11612987fea982e8
Toby McLaughlin <toby@jarpy.net>
2018-06-21 13:59:49 +1000

    Use Mozilla CA cert bundle from the OS for OpenJDK
    Fixes #171

--- tests/test_base_os.py
+++ tests/test_base_os.py
@@ -6,12 +6,6 @@ def test_base_os(host):
     assert host.system_info.release == '7'
 
 
-def test_java_home_env_var(host):
-    java_path_cmdline = '$JAVA_HOME/bin/java -version'
-
-    assert host.run(java_path_cmdline).exit_status == 0
-
-
 def test_no_core_files_exist_in_root(host):
     core_file_check_cmdline = 'ls -l /core*'
 
