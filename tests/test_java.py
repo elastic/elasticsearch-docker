d05cfb63ab94c2dbb93af3f57f32960c2aa0a040
Maxime Gréau <maxime.greau@elastic.co>
2018-10-02 17:30:22 +0200

    [test] Fix test java_major_version for JDK 11 (#202)
    * [test] Fix test java_major_version for JDK 11
    
    We have updated the Docker image to use JDK 11.
    This JDK does not contain a `.` in its version
    
    ```
    docker run -ti --rm --entrypoint=sh docker.elastic.co/elasticsearch/elasticsearch:7.0.0-alpha1-SNAPSHOT
    
    sh-4.2# $JAVA_HOME/bin/java -version
    openjdk version "11" 2018-09-25
    ```
    This commit fixes the test by using a regular expression to check the JDK version

--- tests/test_java.py
+++ tests/test_java.py
@@ -1,9 +1,14 @@
+import re
+
+
 def test_java_is_runnable_via_java_home_env_var(host):
     assert host.run('$JAVA_HOME/bin/java -version').exit_status == 0
 
 
 def test_java_major_version(host):
-    assert 'openjdk version "11.' in host.run('$JAVA_HOME/bin/java -version').stderr
+    version = host.run('$JAVA_HOME/bin/java -version').stderr
+    regexp = r"openjdk version \"11((\.0)*\.[1-9][0-9]*)*"
+    assert re.match(regexp, version)
 
 
 def test_java_uses_the_os_provided_keystore(host):
